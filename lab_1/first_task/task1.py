ec483f120bf6af999b8125d2f2faa3729b6afeb3
Nikita <ppdbavyss@vk.com>
2024-03-31 15:58:41 +0400

    task 1 complete

--- lab_1/first_task/task1.py
+++ lab_1/first_task/task1.py
@@ -1,20 +1,30 @@
-from square import punctuation_symbols
+from work_with_json_file import read_path_file
+from square import polybius_square, punctuation_symbols, path
 
 
 def encrypt_with_polybius_square(message: str, square: dict) -> str:
+    """
+      Encrypts a message using a Polybius square.
+      Args:
+          message (str): The message to be encrypted.
+          square (dict): The Polybius square as a dictionary where keys are uppercase letters
+                         and values are their corresponding encrypted symbols.
+      Returns:
+          str: The encrypted message.
+      """
     encrypted_text = ""
     try:
         for char in message:
             if char.upper() in punctuation_symbols:
                 encrypted_text += char
             else:
-                char_lower = char.upper()
+                char_upper = char.upper()
                 try:
-                    encrypted_char = square.get(char_lower)
+                    encrypted_char = square.get(char_upper)
                     if encrypted_char:
                         encrypted_text += "(" + encrypted_char + ")"
                     else:
-                        encrypted_text += char  # Возвращаем исходный символ, если нет соответствия в словаре
+                        encrypted_text += char
                 except Exception as e:
                     print(f"An exception occurred while encrypting character '{char}': {e}")
     except Exception as exception:
@@ -23,15 +33,42 @@ def encrypt_with_polybius_square(message: str, square: dict) -> str:
 
 
 def main() -> None:
-    polybius_square = {"А": "11", "Б": "12", "В": "13", "Г": "14", "Д": "15", "Е": "16",
-                       "Ё": "21", "Ж": "22", "З": "23", "И": "24", "Й": "25", "К": "26",
-                       "Л": "31", "М": "32", "Н": "33", "О": "34", "П": "35", "Р": "36",
-                       "С": "41", "Т": "42", "У": "43", "Ф": "44", "Х": "45", "Ц": "46",
-                       "Ч": "51", "Ш": "52", "Щ": "53", "Ъ": "54", "Ы": "55", "Ь": "56",
-                       "Э": "61", "Ю": "62", "Я": "63", " ": "64"}
+    """
+    Encrypts the text from an input file using a Polybius square and saves the encrypted text to an output file.
 
-    encrypted_message = encrypt_with_polybius_square("Привет мир!", polybius_square)
-    print(encrypted_message)
+    Reads paths data from a JSON file, where 'folder', 'input_text', and 'output_text' keys specify the folder path,
+    input file name, and output file name respectively.
+
+    If paths data is successfully read and all necessary paths are provided, the function reads the input file,
+    encrypts its content using a Polybius square, and saves the encrypted text to the output file.
+
+    Prints success message if encryption and file saving are successful.
+    Prints error messages for file not found and other exceptions.
+    """
+    paths_data = read_path_file(path)
+    if paths_data:
+        folder = paths_data.get("folder", "")
+        input_text = paths_data.get("input_text", "")
+        output_text = paths_data.get("output_text", "")
+
+        if folder and input_text and output_text:
+            try:
+                with open(f"{folder}/{input_text}", "r", encoding="utf-8") as file:
+                    text = file.read()
+                    encrypted_text = encrypt_with_polybius_square(text, polybius_square)
+
+                with open(f"{folder}/{output_text}", "w", encoding="utf-8") as file:
+                    file.write(encrypted_text)
+
+                print(f"Message encryption success and saved into {output_text}.")
+            except FileNotFoundError:
+                print("File not found.")
+            except Exception as e:
+                print(f"Fail - : {e}")
+        else:
+            print("Couldn't get file paths from JSON data.")
+    else:
+        print("Data could not be read from the JSON file.")
 
 
 if __name__ == "__main__":
