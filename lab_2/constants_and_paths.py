d07ce05cd611d0de2770da6432b27dc5203c2875
Nikita <ppdbavyss@vk.com>
2024-04-24 10:56:22 +0400

    add first frequency test

--- lab_2/constants_and_paths.py
+++ lab_2/constants_and_paths.py
@@ -1,2 +1,4 @@
 CPP = "random_cpp_sequence.txt"
 JAVA = "random_java_sequence.txt"
+BLOCK_SIZE = 8
+PI_VALUES = [0.2148, 0.3672, 0.2305, 0.1875]
--- lab_2/main.py
+++ lab_2/main.py
@@ -1,7 +1,18 @@
+from math import sqrt
+from scipy.special import erfc
 from constants_and_paths import CPP, JAVA
 
 
 def get_sequence(file_name):
+    """
+    Read and return the content(binary sequence) of a file .
+
+    Args:
+        file_name (str): Name of the file to read.
+
+    Returns:
+        str: Content of the file.
+    """
     try:
         with open(file_name, 'r') as file:
             content = file.read()
@@ -14,11 +25,29 @@ def get_sequence(file_name):
         return None
 
 
+def freq_bit_test(sequence: str) -> float:
+    """
+    Calculate the frequency test for a binary sequence.
+
+    Args:
+        sequence (str): A binary sequence to be tested.
+
+    Returns:
+        float: The p-value calculated for the test.
+    """
+    try:
+        p_value = erfc((sum(1 if bit == "1" else -1 for bit in sequence) / sqrt(len(sequence))) / sqrt(2))
+        return p_value
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return -1
+
+
 def main():
     cpp_sequence = get_sequence(CPP)
     java_sequence = get_sequence(JAVA)
-    print(cpp_sequence)
-    print(java_sequence)
+    print(freq_bit_test(cpp_sequence))
+    print(freq_bit_test(java_sequence))
 
 
 if __name__ == "__main__":
